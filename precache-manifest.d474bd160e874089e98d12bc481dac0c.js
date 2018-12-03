d92b8c64ae180431d306f25d3115ee6e1dff841d
Nikhil2591 <Nikhil_369@hotmail.co.uk>
2018-12-03 16:03:20 +0000

    Updates

---
+++ precache-manifest.d474bd160e874089e98d12bc481dac0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dd574dc71532402377",
+    "url": "/Contact-Manager/static/js/runtime~main.65dd574d.js"
+  },
+  {
+    "revision": "f4242783463edeccd4c7",
+    "url": "/Contact-Manager/static/js/main.f4242783.chunk.js"
+  },
+  {
+    "revision": "36e91534776bfdd70955",
+    "url": "/Contact-Manager/static/js/1.36e91534.chunk.js"
+  },
+  {
+    "revision": "f4242783463edeccd4c7",
+    "url": "/Contact-Manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "36e91534776bfdd70955",
+    "url": "/Contact-Manager/static/css/1.ef46235e.chunk.css"
+  },
+  {
+    "revision": "1323558b5663c57badd55955350ec899",
+    "url": "/Contact-Manager/index.html"
+  }
+];
