0e19442a1fe3f4e6bc85e82efbe1c045f48b1db3
Nikhil2591 <Nikhil_369@hotmail.co.uk>
2018-12-03 15:56:39 +0000

    initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    state = {
+        title: '',
+        body: ''
+    };
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(data => this.setState({
+            title:data.title,
+            body: data.body
+        })
+        );
+    }
+
+    // componentWillMount() {
+    //     console.log('componentWillMount...');
+    // }
+
+    // componentDidUpdate() {
+    //     console.log('componentDidUpdate...')
+    // }
+
+    // componentWillUodate() {
+    //     console.log('componentWillUpdate...')
+    // }
+
+    // componentWillReceiveProps(nextProps, nextState) {
+    //     console.log('componentWillReceiveProps...')
+    // }
+
+    // static getDerivedSTateFromProps(nextProps, prevState) {
+    //     return {
+    //         test: 'something'
+    //     };
+    // }
+
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     console.log('getSnapshotBeforeUpdate...')
+    // }
+
+    render() {
+        const { title, body } = this.state;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h1>{body}</h1>
+            </div>
+        );
+    }
+}
+
+export default Test;
